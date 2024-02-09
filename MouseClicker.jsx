e5729a13de04e6e812cc542a6e70a71c6f7c9b3a
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-02-09 13:12:09 +0100

    Add files via upload

--- MouseClicker.jsx
+++ MouseClicker.jsx
@@ -1,10 +1,11 @@
 function MouseClicker({name}) {
     function handleButtonClick(event){
-        console.log(event.currentTarget.name)
+        const imageSrc = event.currentTarget.querySelector("img").src;
+        console.log(imageSrc);
     }
   return (
     <div>
-        <button name={name} onClick={handleButtonClick}>Click Me!</button>
+        <button name={name} onClick={handleButtonClick}><img src="https://i.pinimg.com/736x/fa/cc/e6/facce6ffa1e95f3ec5ea68a6721009ad.jpg" width="50"></img>Click Me!</button>
         {/* <button name="two" onClick={handleButtonClick}>Click Me!</button> */}
     </div>
   )
