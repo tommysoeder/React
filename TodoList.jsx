f9ab782b42e2e0d9e4faf6a542869ed6151487a1
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-02-22 09:29:20 +0100

    Add files via upload

--- TodoList.jsx
+++ TodoList.jsx
@@ -13,11 +13,16 @@ const TodoList = () => {
     setTodoInput('');
   };
 
+  const handleResetTodos = () => {
+    setTodos([]);
+  };
+
   return (
     <div>
       <h2>Todo List</h2>
       <input type="text" value={todoInput} onChange={(e) => setTodoInput(e.target.value)}/>
       <button onClick={handleAddTodo}>Add a ToDo</button>
+      <button onClick={handleResetTodos}>Reset Todos</button>
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>{todo}</li>
